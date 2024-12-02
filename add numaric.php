126da5098a2e7432ed445f8457d591a0d641de5d
rajamehboob <mahboobrehman@ymail.com>
2024-12-02 23:41:51 +0500

    Update add numaric.php

--- add numaric.php
+++ add numaric.php
@@ -1,13 +1,8 @@
 <?php
-// Define two numbers
 $a = 10;
 $b = 5;
-
-// Perform arithmetic operations
 $addition = $a + $b;
 $subtraction = $a - $b;
-
-// Output the results
 echo "Addition: $a + $b = $addition\n";
 echo "Subtraction: $a - $b = $subtraction\n";
 ?>
