e070c1679b5f4c3e852385a5d509580160746b02
rajamehboob <mahboobrehman@ymail.com>
2024-12-02 23:11:36 +0500

    Create  add numaric.php

---
+++ add numaric.php
@@ -0,0 +1,11 @@
+<?php
+// Declare variables
+$number1 = 10;
+$number2 = 20;
+
+// Add the variables
+$sum = $number1 + $number2;
+
+// Output the result
+echo "The sum of $number1 and $number2 is $sum.";
+?>
