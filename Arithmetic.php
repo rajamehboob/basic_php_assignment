fc81c0afad57767e321080d1c2d3916bee4450cd
rajamehboob <mahboobrehman@ymail.com>
2024-12-03 23:02:35 +0500

    Create Arithmetic.php

---
+++ Arithmetic.php
@@ -0,0 +1,20 @@
+<?php
+// Define two variables
+$num1 = 15;
+$num2 = 5;
+
+// Perform arithmetic operations
+$addition = $num1 + $num2;
+$subtraction = $num1 - $num2;
+$multiplication = $num1 * $num2;
+$division = $num1 / $num2;
+
+// Display the results
+echo "Number 1: $num1\n";
+echo "Number 2: $num2\n\n";
+
+echo "Addition: $num1 + $num2 = $addition\n";
+echo "Subtraction: $num1 - $num2 = $subtraction\n";
+echo "Multiplication: $num1 * $num2 = $multiplication\n";
+echo "Division: $num1 / $num2 = $division\n";
+?>
